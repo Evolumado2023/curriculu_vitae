760bed2644f69d21a73245e9b052fd42bf94fabc
Darlison1398 <drsilva123jd@gmail.com>
2023-11-22 00:22:08 -0300

    atualizado 22-11-2023

---
+++ scripts/port.js
@@ -0,0 +1,7 @@
+window.document.getElementById('back').addEventListener('click', function() {
+    window.location = "../index.html";
+})
+
+function wordpress(){
+    window.location = "http://askdrsilva.wordpress.com";
+}
---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+
+function whats(){
+    window.open("https://wa.me/5511954201792?text=Olá, Darlison Silva. Achei seu número no seu site e resolvi entrar em contato. Podemos conversar?");
+}
+
+
+
+function instagram(){
+    window.open("https://www.instagram.com/darlison.souza.9?utm_source=qr&igshid=MzNINGkZWQ4Mg%3D%3D") ;
+}
+
+function facebook(){
+    window.open("https://www.facebook.com/drums.souza.9");
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/darlison-silva-86113a191");
+}
+
+function github(){
+    window.open("https://github.com/Darlison1398");
+}
+
+function wordpress(){
+    window.document.getElementById("direct-wordpress");
+    window.location = "http://askdrsilva.wordpress.com";
+}
