405221d7f7ad1f10b590729ab97808cc6c9b13ce
Darlison1398 <drsilva123jd@gmail.com>
2024-02-11 15:43:04 -0300

    Atualizado 11-02-2024

--- scripts/port.js
+++ scripts/port.js
@@ -27,3 +27,15 @@ function hideText() {
 function github(){
     window.open("https://github.com/Darlison1398");
 }
+
+function instagram(){
+    window.open("https://www.instagram.com/darlison.souza.9?utm_source=qr&igshid=MzNINGkZWQ4Mg%3D%3D") ;
+}
+
+function facebook(){
+    window.open("https://www.facebook.com/drums.souza.9");
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/darlison-silva-86113a191");
+}
