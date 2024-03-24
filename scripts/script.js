bb4b461d2468d338a355b2f3420e9df3ebde9e95
Darlison1398 <drsilva123jd@gmail.com>
2024-03-24 01:06:38 -0300

    Atualizado 24-03-2024

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,10 @@
-
 function whats(){
     window.open("https://wa.me/5511954201792?text=Olá, Darlison Silva. Achei seu número no seu site e resolvi entrar em contato. Podemos conversar?");
 }
 
-
-
-function instagram(){
-    window.open("https://www.instagram.com/darlison.souza.9?utm_source=qr&igshid=MzNINGkZWQ4Mg%3D%3D") ;
-}
-
-function facebook(){
-    window.open("https://www.facebook.com/drums.souza.9");
-}
+window.document.getElementById('redirect').addEventListener('click', () => {
+    window.open("https://drsilva-page-port-dev.netlify.app/");
+})
 
 function linkedin(){
     window.open("https://www.linkedin.com/in/darlison-silva-86113a191");
